028afed206947ea1f5887a68b706fd5736e0bb54
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-05 01:27:01 +0300

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+  $(".header-slider").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/icon/prev.svg" alt="previous"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/icon/next.svg" alt="next"></button>',
+    vertical: true,
+  });
 });
